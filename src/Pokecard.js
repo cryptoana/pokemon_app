2113bdb79c38f5f9ef3164e8fb72736bbca2ead0
oanacodes <oana.carlescu@gmail.com>
2020-01-29 16:31:41 +0100

    changed pokemon image source

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Pokecard.css'
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+
+// Function padToThree takes a number as argument. If the number is lower than 999, we're adding two 0s in front of the number, and then slicing the last 3 numbers. If the number is higher than 999, we return the number.
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number)
 
 class Pokecard extends React.Component {
   render () {
 
-    let imgSrc = `${POKE_API}${this.props.id}.png`
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
 
     return (
       <div className="Pokecard">
