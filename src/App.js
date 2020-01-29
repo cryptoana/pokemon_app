01538e1df219b8be2431f35af6a341406e96c7ce
oanacodes <oana.carlescu@gmail.com>
2020-01-29 17:12:37 +0100

    created Pokegame.js, logic for random pokemon and two separate hands

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Pokedex from './Pokedex'
+import Pokegame from './Pokegame'
 
 class App extends React.Component {
   render () {
     return (
       <div className="App">
         <Pokedex />
+        <Pokegame />
       </div>
     );
   }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -4,7 +4,7 @@ import './Pokecard.css'
 // const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
 
-// Function padToThree takes a number as argument. If the number is lower than 999, we're adding two 0s in front of the number, and then slicing the last 3 numbers. If the number is higher than 999, we return the number.
+// function padToThree takes a number as argument. If the number is lower than 999, we're adding two 0s in front of the number, and then slicing the last 3 numbers. If the number is higher than 999, we return the number.
 let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number)
 
 class Pokecard extends React.Component {
