cb522d21251ee04a4038003de90e16ad4de7cf0f
oanacodes <oana.carlescu@gmail.com>
2020-01-29 13:25:44 +0100

    created Pokecard component, passed props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Pokecard from './Pokecard'
 
 class App extends React.Component {
   render () {
     return (
       <div className="App">
-        
+        <Pokecard 
+          id={4}
+          name="Charmander"
+          type="fire"
+          XP={62}
+        />
       </div>
     );
   }
